46c8ab69018b8f80c4ae80e1220185b768315127
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-05 00:21:49 +0200

    minor changes, moved window.add to bottom

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.addEventListener("keydown", function (e) {
+
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
   if (!audio) return;
@@ -7,7 +8,7 @@ window.addEventListener("keydown", function (e) {
   key.classList.add("playing");
   key.classList.remove("playing");
   key.classList.toggle("playing");
-});
+};
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
@@ -16,3 +17,5 @@ function removeTransition (e) {
     if(e.propertyName !== 'transform') return; // skip it if it's not a transform
     this.classList.remove('playing');
 }
+
+window.addEventListener("keydown", playSound);
