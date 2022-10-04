31995f51cde4ae3b2a5ffcb6897c3804044bcfb3
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-05 00:15:54 +0200

    added some more js, eventhandlers and such

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
-  if(!audio) return;
+  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+  if (!audio) return;
   audio.currentTime = 0;
-  audio.play()
+  audio.play();
+  key.classList.add("playing");
+  key.classList.remove("playing");
+  key.classList.toggle("playing");
 });
 
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+function removeTransition (e) {
+    if(e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('playing');
+}
